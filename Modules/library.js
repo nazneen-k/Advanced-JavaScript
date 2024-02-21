f3ffcb87a769cd4f9e3d80e7fbac085af19e8126
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2024-02-21 20:00:37 +0530

    Add files via upload

---
+++ Modules/library.js
@@ -0,0 +1,24 @@
+// export let message = "ES6 Modules";
+
+// export function user(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// export class test {
+//   constructor() {
+//     console.log("Constructor Method");
+//   }
+// }
+
+// // Export can be done in a single line like this:
+// // export{message,user,test}
+
+
+
+
+
+// ***Using Default*** 
+
+export default function(){
+    console.log("Hello");
+}
---
+++ Modules/main.js
@@ -0,0 +1,24 @@
+// import { message,user as us,test } from "./library.js";
+
+// console.log(message)
+
+// us("Yahoooo Baaba!")
+
+// let a=new test();
+
+
+// /*
+// import * as yahoo from "./library.js"
+
+
+// console.log(yahoo.message);
+// console.log(yahoo.user("Yahoo baaba"));
+// let = new yahoo.test();
+//  */
+
+
+// ***Using Default****
+
+import yahoo from "./library.js"
+
+yahoo();
